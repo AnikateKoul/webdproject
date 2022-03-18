b457cec8645562057dbb91d51d00fe2fea722a0c
AnikateKoul <lcs2021012@iiitl.ac.in>
2022-03-18 17:53:49 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Navbar from './Components/Navbar';
+import Slider from './Components/Slider/Slider'
+
+
+function App() {
+  return (
+    <>
+    <Navbar />
+      <Slider />
+    </>
+  );
+}
+
+export default App;
