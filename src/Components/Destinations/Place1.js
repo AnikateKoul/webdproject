e561f644962d37d7787e540bea277fe2378e22e5
AnikateKoul <lcs2021012@iiitl.ac.in>
2022-03-29 16:04:19 +0530

    22nd commit as of 29/03/2022

--- src/Components/Destinations/Place1.js
+++ src/Components/Destinations/Place1.js
@@ -1,48 +1,34 @@
 import React from "react";
-import './Place.css'
+// import "./Place.css";
 
 export default function Place1() {
   return (
     <>
-    <div style={{ marginTop: "70px" }}></div>
-    <h1>Shimla</h1>
-    <div className="main">
-        <img src="https://images.unsplash.com/photo-1536869338989-e7ffd2297454?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8cGVvcGxlJTIwYXQlMjBiZWFjaHxlbnwwfHwwfHw%3D&w=1000&q=80" alt=""/>
-        <div className="bottom-left"><h1>Bottom Right</h1></div>
-    </div>
-    <div className="cards">
-        <div className="card ">
-            <div className="image "><img src="https://i.pinimg.com/originals/7f/62/8d/7f628daeff85825989a0d30a89d7b260.jpg" alt=""/></div>
-            <div className="text">
-                <h1 className="">Shimla</h1>
-                <p className="" >
-                    Sub Heading-1
-                </p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit a dolorem repellendus eius, pariatur facere rerum perspiciatis ex tenetur sit harum veniam, ad quos nobis sunt iste iusto velit mollitia nemo cupiditate nisi, possimus quaerat. Harum, ipsa? Blanditiis, maiores ipsa voluptates nisi doloribus est modi vero fugiat tenetur soluta fugit amet excepturi voluptatum nostrum molestias repudiandae dicta illum sit dignissimos. Commodi mollitia repellendus veniam dignissimos molestias nemo, eaque perspiciatis quod amet cupiditate repudiandae officiis assumenda, minima velit! Recusandae quaerat sapiente, sed et sequi nam ea reprehenderit impedit quas quod, adipisci architecto voluptas. Praesentium beatae sit, error dolorum at sint. Similique!</p>
-            </div>
+      <div style={{ marginTop: "70px" }}></div>
+      <div className="cards">
+        <div className="row container-fluid">
+            <img
+              src="https://i.pinimg.com/originals/7f/62/8d/7f628daeff85825989a0d30a89d7b260.jpg"
+              alt=""
+              className="img-fluid float-start"
+            />
         </div>
-        <div className="card ">
-            <div className="image "><img src="https://i.pinimg.com/originals/7f/62/8d/7f628daeff85825989a0d30a89d7b260.jpg" alt=""/></div>
-            <div className="text">
-                <h1 className="">Goa</h1>
-                <p className="" >
-                    Sub Heading-2
-                </p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit a dolorem repellendus eius, pariatur facere rerum perspiciatis ex tenetur sit harum veniam, ad quos nobis sunt iste iusto velit mollitia nemo cupiditate nisi, possimus quaerat. Harum, ipsa? Blanditiis, maiores ipsa voluptates nisi doloribus est modi vero fugiat tenetur soluta fugit amet excepturi voluptatum nostrum molestias repudiandae dicta illum sit dignissimos. Commodi mollitia repellendus veniam dignissimos molestias nemo, eaque perspiciatis quod amet cupiditate repudiandae officiis assumenda, minima velit! Recusandae quaerat sapiente, sed et sequi nam ea reprehenderit impedit quas quod, adipisci architecto voluptas. Praesentium beatae sit, error dolorum at sint. Similique!</p>
-            </div>
+        <div className="container text-center row">
+        <h1>Shimla</h1>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit a
+          dolorem repellendus eius, pariatur facere rerum perspiciatis ex
+          tenetur sit harum veniam, ad quos nobis sunt iste iusto velit mollitia
+          nemo cupiditate nisi, possimus quaerat. Harum, ipsa? Blanditiis,
+          maiores ipsa voluptates nisi doloribus est modi vero fugiat tenetur
+          soluta fugit amet excepturi voluptatum nostrum molestias repudiandae
+          dicta illum sit dignissimos. Commodi mollitia repellendus veniam
+          dignissimos molestias nemo, eaque perspiciatis quod amet cupiditate
+          repudiandae officiis assumenda, minima velit! Recusandae quaerat
+          sapiente, sed et sequi nam ea reprehenderit impedit quas quod,
+          adipisci architecto voluptas. Praesentium beatae sit, error dolorum at
+          sint. Similique!
         </div>
-        <div className="card ">
-            <div className="image "><img src="https://images.unsplash.com/photo-1583143874828-de3d288be51a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bXVzc29vcmllJTJDJTIwaW5kaWF8ZW58MHx8MHx8&w=1000&q=80" alt=""/></div>
-            <div className="text">
-                <h1 className="">Masoori</h1>
-                <p className="" >
-                    Sub Heading-3
-                </p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit a dolorem repellendus eius, pariatur facere rerum perspiciatis ex tenetur sit harum veniam, ad quos nobis sunt iste iusto velit mollitia nemo cupiditate nisi, possimus quaerat. Harum, ipsa? Blanditiis, maiores ipsa voluptates nisi doloribus est modi vero fugiat tenetur soluta fugit amet excepturi voluptatum nostrum molestias repudiandae dicta illum sit dignissimos. Commodi mollitia repellendus veniam dignissimos molestias nemo, eaque perspiciatis quod amet cupiditate repudiandae officiis assumenda, minima velit! Recusandae quaerat sapiente, sed et sequi nam ea reprehenderit impedit quas quod, adipisci architecto voluptas. Praesentium beatae sit, error dolorum at sint. Similique!</p>
-            </div>
-        </div>
-    </div>
-
+      </div>
     </>
   );
 }
--- src/Components/footer.js
+++ src/Components/footer.js
@@ -47,7 +47,7 @@ export default function Footer(props) {
             <div className="row mt-3">
               <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
                 <h6 className="text-uppercase fw-bold mb-4">
-                  <i className="fas fa-gem me-3"></i>Company name
+                  <i className="fas fa-gem me-3"></i> Company name
                 </h6>
                 <p>
                   Here you can use rows and columns to organize your footer
