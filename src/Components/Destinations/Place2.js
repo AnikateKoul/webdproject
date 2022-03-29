1bc79f29cac556e84cdfa4bc8bef35a720090cef
Achal <achalg1708@gmail.com>
2022-03-29 17:40:56 +0530

    23rd commit 3/29/2022

--- src/Components/Destinations/Place2.js
+++ src/Components/Destinations/Place2.js
@@ -1,32 +1,94 @@
 import React from "react";
-// import "./Place.css";
+import "./Place.css";
 
 export default function Place2() {
   return (
     <>
-    <div style={{ marginTop: "70px" }}></div>
+      <div style={{ marginTop: "70px" }}></div>
+      <div className="main">
+        <img
+          src="https://images.unsplash.com/photo-1512343879784-a960bf40e7f2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
+          alt=""
+        />
+      </div>
       <div className="cards">
-        <div className="row container-fluid">
-          <img
-            src="https://images.unsplash.com/photo-1512343879784-a960bf40e7f2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
-            alt=""
-            className="img-fluid float-start"
-          />
+        <div className="card ">
+          <div className="image ">
+            <img
+              src="https://i.pinimg.com/originals/7f/62/8d/7f628daeff85825989a0d30a89d7b260.jpg"
+              alt=""
+            />
+          </div>
+          <div className="text">
+            <h1 className="">Shimla</h1>
+            <p className="">Sub Heading-1</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit a
+              dolorem repellendus eius, pariatur facere rerum perspiciatis ex
+              tenetur sit harum veniam, ad quos nobis sunt iste iusto velit
+              mollitia nemo cupiditate nisi, possimus quaerat. Harum, ipsa?
+              Blanditiis, maiores ipsa voluptates nisi doloribus est modi vero
+              fugiat tenetur soluta fugit amet excepturi voluptatum nostrum
+              molestias repudiandae dicta illum sit dignissimos. Commodi
+              mollitia repellendus veniam dignissimos molestias nemo, eaque
+              perspiciatis quod amet cupiditate repudiandae officiis assumenda,
+              minima velit! Recusandae quaerat sapiente, sed et sequi nam ea
+              reprehenderit impedit quas quod, adipisci architecto voluptas.
+              Praesentium beatae sit, error dolorum at sint. Similique!
+            </p>
+          </div>
         </div>
-        <div className="container text-center row">
-        <h1>Shimla</h1>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit a
-          dolorem repellendus eius, pariatur facere rerum perspiciatis ex
-          tenetur sit harum veniam, ad quos nobis sunt iste iusto velit mollitia
-          nemo cupiditate nisi, possimus quaerat. Harum, ipsa? Blanditiis,
-          maiores ipsa voluptates nisi doloribus est modi vero fugiat tenetur
-          soluta fugit amet excepturi voluptatum nostrum molestias repudiandae
-          dicta illum sit dignissimos. Commodi mollitia repellendus veniam
-          dignissimos molestias nemo, eaque perspiciatis quod amet cupiditate
-          repudiandae officiis assumenda, minima velit! Recusandae quaerat
-          sapiente, sed et sequi nam ea reprehenderit impedit quas quod,
-          adipisci architecto voluptas. Praesentium beatae sit, error dolorum at
-          sint. Similique!
+        <div className="card ">
+          <div className="image ">
+            <img
+              src="https://i.pinimg.com/originals/7f/62/8d/7f628daeff85825989a0d30a89d7b260.jpg"
+              alt=""
+            />
+          </div>
+          <div className="text">
+            <h1 className="">Shimla</h1>
+            <p className="">Sub Heading-2</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit a
+              dolorem repellendus eius, pariatur facere rerum perspiciatis ex
+              tenetur sit harum veniam, ad quos nobis sunt iste iusto velit
+              mollitia nemo cupiditate nisi, possimus quaerat. Harum, ipsa?
+              Blanditiis, maiores ipsa voluptates nisi doloribus est modi vero
+              fugiat tenetur soluta fugit amet excepturi voluptatum nostrum
+              molestias repudiandae dicta illum sit dignissimos. Commodi
+              mollitia repellendus veniam dignissimos molestias nemo, eaque
+              perspiciatis quod amet cupiditate repudiandae officiis assumenda,
+              minima velit! Recusandae quaerat sapiente, sed et sequi nam ea
+              reprehenderit impedit quas quod, adipisci architecto voluptas.
+              Praesentium beatae sit, error dolorum at sint. Similique!
+            </p>
+          </div>
+        </div>
+        <div className="card ">
+          <div className="image ">
+            <img
+              src="https://images.unsplash.com/photo-1583143874828-de3d288be51a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bXVzc29vcmllJTJDJTIwaW5kaWF8ZW58MHx8MHx8&w=1000&q=80"
+              alt=""
+            />
+          </div>
+          <div className="text">
+            <h1 className="">Masoori</h1>
+            <p className="">Sub Heading-3</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit a
+              dolorem repellendus eius, pariatur facere rerum perspiciatis ex
+              tenetur sit harum veniam, ad quos nobis sunt iste iusto velit
+              mollitia nemo cupiditate nisi, possimus quaerat. Harum, ipsa?
+              Blanditiis, maiores ipsa voluptates nisi doloribus est modi vero
+              fugiat tenetur soluta fugit amet excepturi voluptatum nostrum
+              molestias repudiandae dicta illum sit dignissimos. Commodi
+              mollitia repellendus veniam dignissimos molestias nemo, eaque
+              perspiciatis quod amet cupiditate repudiandae officiis assumenda,
+              minima velit! Recusandae quaerat sapiente, sed et sequi nam ea
+              reprehenderit impedit quas quod, adipisci architecto voluptas.
+              Praesentium beatae sit, error dolorum at sint. Similique!
+            </p>
+          </div>
         </div>
       </div>
     </>
--- src/Components/Destinations/Place4.js
+++ src/Components/Destinations/Place4.js
@@ -1,34 +1,97 @@
 import React from "react";
-// import "./Place.css";
+import "./Place.css";
 
 export default function Place4() {
   return (
     <>
       <div style={{ marginTop: "70px" }}></div>
+      <div className="main">
+        <img
+          src="https://images.unsplash.com/photo-1566560073340-0ee82cb29074?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8bGFkYWtofGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+          alt=""
+        />
+      </div>
       <div className="cards">
-        <div className="row container-fluid">
-          <img
-            src="https://images.unsplash.com/photo-1566560073340-0ee82cb29074?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8bGFkYWtofGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
-            alt=""
-            className="img-fluid float-start"
-          />
+        <div className="card ">
+          <div className="image ">
+            <img
+              src="https://i.pinimg.com/originals/7f/62/8d/7f628daeff85825989a0d30a89d7b260.jpg"
+              alt=""
+            />
+          </div>
+          <div className="text">
+            <h1 className="">Shimla</h1>
+            <p className="">Sub Heading-1</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit a
+              dolorem repellendus eius, pariatur facere rerum perspiciatis ex
+              tenetur sit harum veniam, ad quos nobis sunt iste iusto velit
+              mollitia nemo cupiditate nisi, possimus quaerat. Harum, ipsa?
+              Blanditiis, maiores ipsa voluptates nisi doloribus est modi vero
+              fugiat tenetur soluta fugit amet excepturi voluptatum nostrum
+              molestias repudiandae dicta illum sit dignissimos. Commodi
+              mollitia repellendus veniam dignissimos molestias nemo, eaque
+              perspiciatis quod amet cupiditate repudiandae officiis assumenda,
+              minima velit! Recusandae quaerat sapiente, sed et sequi nam ea
+              reprehenderit impedit quas quod, adipisci architecto voluptas.
+              Praesentium beatae sit, error dolorum at sint. Similique!
+            </p>
+          </div>
         </div>
-        <div className="container text-center row">
-        <h1>Shimla</h1>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit a
-          dolorem repellendus eius, pariatur facere rerum perspiciatis ex
-          tenetur sit harum veniam, ad quos nobis sunt iste iusto velit mollitia
-          nemo cupiditate nisi, possimus quaerat. Harum, ipsa? Blanditiis,
-          maiores ipsa voluptates nisi doloribus est modi vero fugiat tenetur
-          soluta fugit amet excepturi voluptatum nostrum molestias repudiandae
-          dicta illum sit dignissimos. Commodi mollitia repellendus veniam
-          dignissimos molestias nemo, eaque perspiciatis quod amet cupiditate
-          repudiandae officiis assumenda, minima velit! Recusandae quaerat
-          sapiente, sed et sequi nam ea reprehenderit impedit quas quod,
-          adipisci architecto voluptas. Praesentium beatae sit, error dolorum at
-          sint. Similique!
+        <div className="card ">
+          <div className="image ">
+            <img
+              src="https://i.pinimg.com/originals/7f/62/8d/7f628daeff85825989a0d30a89d7b260.jpg"
+              alt=""
+            />
+          </div>
+          <div className="text">
+            <h1 className="">Shimla</h1>
+            <p className="">Sub Heading-2</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit a
+              dolorem repellendus eius, pariatur facere rerum perspiciatis ex
+              tenetur sit harum veniam, ad quos nobis sunt iste iusto velit
+              mollitia nemo cupiditate nisi, possimus quaerat. Harum, ipsa?
+              Blanditiis, maiores ipsa voluptates nisi doloribus est modi vero
+              fugiat tenetur soluta fugit amet excepturi voluptatum nostrum
+              molestias repudiandae dicta illum sit dignissimos. Commodi
+              mollitia repellendus veniam dignissimos molestias nemo, eaque
+              perspiciatis quod amet cupiditate repudiandae officiis assumenda,
+              minima velit! Recusandae quaerat sapiente, sed et sequi nam ea
+              reprehenderit impedit quas quod, adipisci architecto voluptas.
+              Praesentium beatae sit, error dolorum at sint. Similique!
+            </p>
+          </div>
+        </div>
+        <div className="card ">
+          <div className="image ">
+            <img
+              src="https://images.unsplash.com/photo-1583143874828-de3d288be51a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bXVzc29vcmllJTJDJTIwaW5kaWF8ZW58MHx8MHx8&w=1000&q=80"
+              alt=""
+            />
+          </div>
+          <div className="text">
+            <h1 className="">Masoori</h1>
+            <p className="">Sub Heading-3</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit a
+              dolorem repellendus eius, pariatur facere rerum perspiciatis ex
+              tenetur sit harum veniam, ad quos nobis sunt iste iusto velit
+              mollitia nemo cupiditate nisi, possimus quaerat. Harum, ipsa?
+              Blanditiis, maiores ipsa voluptates nisi doloribus est modi vero
+              fugiat tenetur soluta fugit amet excepturi voluptatum nostrum
+              molestias repudiandae dicta illum sit dignissimos. Commodi
+              mollitia repellendus veniam dignissimos molestias nemo, eaque
+              perspiciatis quod amet cupiditate repudiandae officiis assumenda,
+              minima velit! Recusandae quaerat sapiente, sed et sequi nam ea
+              reprehenderit impedit quas quod, adipisci architecto voluptas.
+              Praesentium beatae sit, error dolorum at sint. Similique!
+            </p>
+          </div>
         </div>
       </div>
     </>
   );
 }
+
