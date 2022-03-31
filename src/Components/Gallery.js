f4f2bbda452693e2910e6a30f327eda5925e4366
Nikhil <lcs2021006@iiitl.ac.in>
2022-03-31 21:05:25 +0530

    added gallery

--- src/Components/Gallery.js
+++ src/Components/Gallery.js
@@ -4,10 +4,10 @@ import "./Gallery.css";
 export default function Gallery(props) {
   return (
       <>
-    <div className="main">
+    <div className="main3">
     <img
         src="https://images.unsplash.com/photo-1544785316-6e58aed68a50?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"/>
-    <div className="main-text">
+    <div className="main3-text">
         <p><b>WELCOME </b>TO THE GALLERY</p>
     </div>
 </div>
