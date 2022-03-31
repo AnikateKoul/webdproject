7763f5a2b53196dab211225a6e9c9e901b214208
Sahil <lcs2021033@iiitl.ac.in>
2022-03-31 14:00:11 +0530

    Modified Sign up form

--- src/Components/SignupForm.js
+++ src/Components/SignupForm.js
@@ -1,16 +1,18 @@
 import { useState } from 'react';
+import './SignupForm.css';
 
 export default function SignupForm() {
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+    const [address, setAddress] = useState("");
+    const [contactno, setContactNo] = useState("");
 
     const handleOnSubmit = async (e) => {
         e.preventDefault();
         let result = await fetch(
         'http://localhost:5000/register', {
             method: "post",
-            body: JSON.stringify({ name, email, password }),
+            body: JSON.stringify({ name, email, address,contactno }),
             headers: {
                 'Content-Type': 'application/json'
             }
@@ -19,23 +21,34 @@ export default function SignupForm() {
         console.warn(result);
         if (result) {
             alert("Data saved successfully");
-            setEmail("");
             setName("");
-            setPassword("");
+            setEmail("");
+            setAddress("");
+            setContactNo("");
         }
     }
     return (
         <>
-            <h1>This is React WebApp </h1>
+            <div className="background">
+              <div className="shape"></div>
+              <div className="shape"></div>
+            </div>
             <form action="">
-                <input type="text" placeholder="name" 
+                <h3>Login Here </h3>
+                <label for="username" >Name</label>
+                <input type="text" placeholder="name" id='username'
                 value={name} onChange={(e) => setName(e.target.value)} />
-                <input type="email" placeholder="email" 
+                <label for="email" required>Email</label>
+                <input type="email" placeholder="email" id='email'
                 value={email} onChange={(e) => setEmail(e.target.value)} />
-                <input type="password" placeholder="password" 
-                value={password} onChange={(e) => setPassword(e.target.value)} />
-                <button type="submit" 
-                onClick={handleOnSubmit}>submit</button>
+                <label for="address" required>Address</label>
+                <input type="text" placeholder="Address" id='address'
+                value={address} onChange={(e) => setAddress(e.target.value)} />
+                <label for="contact" required>Contact No.</label>
+                <input type="text" placeholder="Contact No." id='contactno'
+                value={contactno} onChange={(e) => setContactNo(e.target.value)} />
+                <button type="submit" id='sub'
+                onClick={handleOnSubmit}>Sign Up</button>
             </form>
 
         </>
