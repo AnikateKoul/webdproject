df0512ad148249e660260549488243996d1a4be9
Sahil <lcs2021033@iiitl.ac.in>
2022-04-02 02:42:55 +0530

    Changed size of font size in slider

--- src/Components/Slider.js
+++ src/Components/Slider.js
@@ -13,7 +13,7 @@ export default function Slider(props) {
       src="https://images.unsplash.com/photo-1641933002719-4c878c83f48e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
       alt="First slide"
     /></Link>
-    <Carousel.Caption>
+    <Carousel.Caption className='carouselContent'>
       <h3>Shimla</h3>
       <p>Live with no excuses and travel with no regrets.</p>
     </Carousel.Caption>
@@ -24,7 +24,7 @@ export default function Slider(props) {
       src="https://images.unsplash.com/photo-1526761122248-c31c93f8b2b9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1173&q=80"
       alt="Second slide"
     /></Link>
-    <Carousel.Caption>
+    <Carousel.Caption className='carouselContent'>
       <h3>Goa</h3>
       <p>We travel not to escape life but for life not to escape us.</p>
     </Carousel.Caption>
@@ -35,7 +35,7 @@ export default function Slider(props) {
       src="https://images.unsplash.com/photo-1500614922032-b6dd337b1313?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
       alt="Third slide"
     /></Link>
-    <Carousel.Caption>
+    <Carousel.Caption className='carouselContent'>
       <h3>Ooty</h3>
       <p>Life is either a daring adventure or nothing at all.</p>
     </Carousel.Caption>
@@ -46,7 +46,7 @@ export default function Slider(props) {
       src="https://images.unsplash.com/photo-1593118845043-359e5f628214?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
       alt="Third slide"
     /></Link>
-    <Carousel.Caption>
+    <Carousel.Caption className='carouselContent'>
       <h3>Ladakh</h3>
       <p>Traveling – it leaves you speechless, then turns you into a storyteller.</p>
     </Carousel.Caption>
@@ -57,7 +57,7 @@ export default function Slider(props) {
       src="https://images.unsplash.com/photo-1577089907583-991f1ba4a03c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
       alt="Third slide"
     /></Link>
-    <Carousel.Caption>
+    <Carousel.Caption className='carouselContent'>
       <h3>Jaisalmer</h3>
       <p>Stop worrying about the potholes in the road and enjoy the journey</p>
     </Carousel.Caption>
