3a495ae3e65c92817448939ea0d00fa64836e97d
Achal <achalg1708@gmail.com>
2022-04-01 19:52:11 +0530

    91st commit 4/01/2022

--- src/Components/SignupForm.js
+++ src/Components/SignupForm.js
@@ -5,6 +5,7 @@ export default function SignupForm(props) {
     let myStyle = {
         color: props.mode === "dark" ? "white" : "#212529",
         backgroundColor: props.mode === "dark" ? "#212529" : "white",
+        border: props.mode === "dark" ? "white" : "#212529",
       };
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
@@ -41,16 +42,16 @@ export default function SignupForm(props) {
             <form action="" style={myStyle}>
                 <h3 style={myStyle}>Login Here </h3>
                 <label className='SUlabel' for="username" style={myStyle}>Name</label>
-                <input type="text" placeholder="Name" id='username'
+                <input type="text" style={{border: '1px solid black',color: 'black'}} placeholder="Name" id='username'
                 value={name} onChange={(e) => setName(e.target.value)} />
                 <label className='SUlabel' for="email" required style={myStyle}>Email</label>
-                <input type="email" placeholder="Email" id='email'
+                <input type="email" style={{border: '1px solid black',color: 'black'}} placeholder="Email" id='email'
                 value={email} onChange={(e) => setEmail(e.target.value)} />
                 <label className='SUlabel' for="address" required style={myStyle}>Address</label>
-                <input type="text" placeholder="Address" id='address'
+                <input type="text" style={{border: '1px solid black',color: 'black'}} placeholder="Address" id='address'
                 value={address} onChange={(e) => setAddress(e.target.value)} />
                 <label className='SUlabel' for="contact" required style={myStyle}>Contact No.</label>
-                <input type="text" placeholder="Contact No." id='contactno'
+                <input type="text" style={{border: '1px solid black',color: 'black'}} placeholder="Contact No." id='contactno'
                 value={contactno} onChange={(e) => setContactNo(e.target.value)} />
                 <button type="submit" id='sub'
                 onClick={handleOnSubmit}>Sign Up</button>
